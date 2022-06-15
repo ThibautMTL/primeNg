346d205e801a14d7c0045ada35492e0a90c28fd8
Thibaut <thibaut@tractr.net>
2022-06-15 14:15:22 +0200

    feat: test dropdown

--- apps/prime/src/app/app.component.ts
+++ apps/prime/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface City {
+  label: string,
+  value: string
+}
+
 @Component({
   selector: 'primeng-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prime';
+
+  valueKnob = 50;
+
+  cities: City[] = [
+    { label: 'New York', value: 'NY' },
+    { label: 'Rome', value: 'RM' },
+    { label: 'London', value: 'LDN' },
+    { label: 'Istanbul', value: 'IST' },
+    { label: 'Paris', value: 'PRS' }
+  ];
+
+  citySelect?: City;
+
+  displaySelect(city: City | Date) {
+    console.log(city);
+  }
 }
--- apps/prime/src/app/app.module.ts
+++ apps/prime/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { KnobModule } from 'primeng/knob';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const compPrime = [
-  ButtonModule
+  ButtonModule,
+  CalendarModule,
+  KnobModule,
+  DropdownModule
 ]
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ...compPrime],
+  imports: [BrowserModule, BrowserAnimationsModule, ...compPrime, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
